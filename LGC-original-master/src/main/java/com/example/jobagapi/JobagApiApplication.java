76026a2b41bd1a74cc40e4b8ea53ad07f71289bd
Caro2870 <carol.vi28@gmail.com>
2021-05-05 05:59:55 -0500

    entidades

---
+++ LGC-original-master/src/main/java/com/example/jobagapi/JobagApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.jobagapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobagApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobagApiApplication.class, args);
+    }
+
+}
---
+++ LGC-original-master/src/test/java/com/example/jobagapi/JobagApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jobagapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobagApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
