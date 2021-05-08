048e5c8a4acdc30aea4037eb437e50f2b3792666
Caro2870 <carol.vi28@gmail.com>
2021-05-08 04:12:30 -0500

    Feature(dev) testing

---
+++ src/test/java/com/example/jobagapi/EmployeerServiceImplTest.java
@@ -0,0 +1,59 @@
+package com.example.jobagapi;
+import com.example.jobagapi.domain.model.Employeer;
+import com.example.jobagapi.domain.repository.EmployeerRepository;
+import com.example.jobagapi.domain.service.EmployeerService;
+import com.example.jobagapi.service.EmployeerServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+public class EmployeerServiceImplTest {
+
+    @MockBean
+    private EmployeerRepository employeerRepository;
+    @Autowired
+    private EmployeerService employeerService;
+
+    @TestConfiguration
+    static class EmployeerServiceImplTestConfiguration
+    {
+        @Bean
+        public EmployeerService employeerService()
+        {
+            return new EmployeerServiceImpl();
+        }
+    }
+    @Test
+    @DisplayName("When getEmployeerById With Valid Title Then Returns Employeer")
+    public void whenGetEmployeerByIdWithValidIdThenReturnsEmployeer() {
+        // Arrange
+        Long Id = 1L;
+        Employeer employeer = new Employeer().setId(Id);
+        when(employeerRepository.findById(Id))
+                .thenReturn(Optional.of(employeer));
+
+        // Act
+        Employeer foundEmployeer = employeerService.getEmployeerById(Id);
+
+        // Assert
+        assertThat(foundEmployeer.getId()).isEqualTo(Id);
+
+    }
+
+
+
+
+
+
+}
