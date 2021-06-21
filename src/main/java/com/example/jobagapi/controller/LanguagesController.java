8180774cfe74d0c460c23e45f6db413b9d3b7207
Caro2870 <55043767+Caro2870@users.noreply.github.com>
2021-06-21 03:13:19 -0500

    Update LanguagesController.java

--- src/main/java/com/example/jobagapi/controller/LanguagesController.java
+++ src/main/java/com/example/jobagapi/controller/LanguagesController.java
@@ -21,6 +21,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@CrossOrigin(origins="http://localhost:4200")
 @RequestMapping("/api")
 public class LanguagesController {
     @Autowired
