d11d855a35e1ceff7621a08ce41c7527e1477380
Caro2870 <55043767+Caro2870@users.noreply.github.com>
2021-06-21 04:28:34 -0500

    Update JobagApiApplication.java

--- src/main/java/com/example/jobagapi/JobagApiApplication.java
+++ src/main/java/com/example/jobagapi/JobagApiApplication.java
@@ -25,7 +25,7 @@ public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:4200");
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200");
 			}
 		};
 	}
