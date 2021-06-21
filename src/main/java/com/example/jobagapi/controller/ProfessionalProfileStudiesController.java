b92a1910569317bd46c82323872213415bda82c5
Caro2870 <55043767+Caro2870@users.noreply.github.com>
2021-06-21 03:15:39 -0500

    Update ProfessionalProfileStudiesController.java

--- src/main/java/com/example/jobagapi/controller/ProfessionalProfileStudiesController.java
+++ src/main/java/com/example/jobagapi/controller/ProfessionalProfileStudiesController.java
@@ -17,6 +17,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins="http://localhost:4200")
 
 public class ProfessionalProfileStudiesController {
 
