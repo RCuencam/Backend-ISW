b7101bd363d70732b8e7fa6c970317de01d106d7
Schmidt <56980361+elcrack2001@users.noreply.github.com>
2021-06-26 21:52:45 -0500

    Update PostulantJobResource.java

--- src/main/java/com/example/jobagapi/resource/PostulantJobResource.java
+++ src/main/java/com/example/jobagapi/resource/PostulantJobResource.java
@@ -4,6 +4,13 @@ public class PostulantJobResource {
     private Long id;
     private boolean aceppt;
 
+    private Long jobOfferId;
+    private String titleJobOffer;
+    private Long postulantId;
+    private String firstNamePostulant;
+    private String lastNamePostulant;
+    private String emailPostulant;
+
     public Long getId() {
         return id;
     }
@@ -19,6 +26,60 @@ public class PostulantJobResource {
 
     public PostulantJobResource setAceppt(boolean aceppt) {
         this.aceppt = aceppt;
+        return this;
+    }
+
+    public Long getJobOfferId() {
+        return jobOfferId;
+    }
+
+    public PostulantJobResource setJobOfferId(Long jobOfferId) {
+        this.jobOfferId = jobOfferId;
+        return this;
+    }
+
+    public String getTitleJobOffer() {
+        return titleJobOffer;
+    }
+
+    public PostulantJobResource setTitleJobOffer(String titleJobOffer) {
+        this.titleJobOffer = titleJobOffer;
+        return this;
+    }
+
+    public Long getPostulantId() {
+        return postulantId;
+    }
+
+    public PostulantJobResource setPostulantId(Long postulantId) {
+        this.postulantId = postulantId;
+        return this;
+    }
+
+    public String getFirstNamePostulant() {
+        return firstNamePostulant;
+    }
+
+    public PostulantJobResource setFirstNamePostulant(String firstNamePostulant) {
+        this.firstNamePostulant = firstNamePostulant;
+        return this;
+    }
+
+    public String getLastNamePostulant() {
+        return lastNamePostulant;
+    }
+
+    public PostulantJobResource setLastNamePostulant(String lastNamePostulant) {
+        this.lastNamePostulant = lastNamePostulant;
+        return this;
+    }
+
+    public String getEmailPostulant() {
+        return emailPostulant;
+    }
+
+    public PostulantJobResource setEmailPostulant(String emailPostulant) {
+        this.emailPostulant = emailPostulant;
         return this;
     }
 }
