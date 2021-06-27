26b6961f5e210cd3e8fe8599b5525bae1b73a6e4
Schmidt <56980361+elcrack2001@users.noreply.github.com>
2021-06-26 19:43:55 -0500

    Update JobOfferService.java

--- src/main/java/com/example/jobagapi/domain/service/JobOfferService.java
+++ src/main/java/com/example/jobagapi/domain/service/JobOfferService.java
@@ -8,7 +8,9 @@ import org.springframework.http.ResponseEntity;
 public interface JobOfferService {
     Page<JobOffer> getAllJobOffersByEmployeerId(Long employeerId, Pageable pageable);
     JobOffer getJobOfferByIdAndEmployeerId(Long employeerId, Long jobOfferId);
+    JobOffer getJobOfferById(Long jobOfferId);
     JobOffer createJobOffer(Long employeerId, JobOffer jobOffer);
     JobOffer updateJobOffer(Long employeerId, Long jobOfferId, JobOffer jobOfferDetails);
+    Page<JobOffer> getAllJobOffer(Pageable pageable);
     ResponseEntity<?> deleteJobOffer(Long employeerId, Long jobOfferId);
 }
