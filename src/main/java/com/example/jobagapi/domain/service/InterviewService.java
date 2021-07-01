1c534f06334e4a0c830059134f6859571aa74681
Schmidt <56980361+elcrack2001@users.noreply.github.com>
2021-07-01 17:17:16 -0500

    Update InterviewService.java

--- src/main/java/com/example/jobagapi/domain/service/InterviewService.java
+++ src/main/java/com/example/jobagapi/domain/service/InterviewService.java
@@ -1,26 +1,16 @@
 package com.example.jobagapi.domain.service;
 
 import com.example.jobagapi.domain.model.Interview;
-import com.example.jobagapi.domain.model.JobOffer;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 
 public interface InterviewService {
-    //Creacion de la entrevista con el postulante y la oferta de trabajo
     Interview createInterview(Long postulantId, Long jobOfferId, Interview interview);
-    //Modificacion de la entrevista
-    Interview updateInterview(Long postulantId, Long jobOfferId, Long interviewId, Interview interviewDetails);
-    //Eliminacion de la entrevista
-    ResponseEntity<?> deleteInterview(Long postulantId, Long jobOfferId, Long interviewId);
-    //Retorna la entrevista segun Id del postulante
-    Page<Interview> getAllInterviewsByPostulantId(Long postulantId, Pageable pageable);
-    //Retorna la entrevista segun Id de la oferta de trabajo
-    Page<Interview> getAllInterviewsByJobOfferId(Long jobOfferId, Pageable pageable);
-    //Retorna todas las entrevistas segun el  id del postulante y de la oferta de trabajo
-    Page<Interview> getAllInterviewByPostulantIdAndJobOfferId(Long postulantId, Long jobOfferId, Pageable pageable);
-
+    Interview updateInterview(Long postulantId, Long jobOfferId, Interview interviewDetails);
+    ResponseEntity<?> deleteInterview(Long postulantId, Long jobOfferId);
     Page<Interview> getAllInterview(Pageable pageable);
+    Page<Interview> getAllInterviewsByPostulantId(Long postulantId, Pageable pageable);
+    Page<Interview> getAllInterviewsByJobOfferId(Long jobOfferId, Pageable pageable);
     Interview getInterviewById(Long interviewId);
-
 }
