805d8bb07a84b39492018dd1298d2ed7145bc077
Schmidt <56980361+elcrack2001@users.noreply.github.com>
2021-07-01 15:17:30 -0500

    Update CompanyResource.java

--- src/main/java/com/example/jobagapi/resource/CompanyResource.java
+++ src/main/java/com/example/jobagapi/resource/CompanyResource.java
@@ -2,68 +2,170 @@ package com.example.jobagapi.resource;
 
 public class CompanyResource {
 
-    Long id;
-    String name;
-    String description;
-    String sector;
-    String logo;
-    Long ruc;
-    String dirección;
+    private Long id;
+    private String name;
+    private String description;
+    private String logo;
+    private Long ruc;
+    private String dirección;
 
+    //Employeer
+    private Long idEmployeer;
+    private String firstnameEmployeer;
+    private String lastnameEmployeer;
+    private String emailEmployeer;
+    private Long numberEmployeer;
+    private String passwordEmployeer;
+    private String documentEmployeer;
+
+    //Sector
+
+    private Long idSector;
+    private String nameSector;
+    private String descriptionSector;
 
     public Long getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public CompanyResource setId(Long id) {
         this.id = id;
+        return this;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
+    public CompanyResource setName(String name) {
         this.name = name;
+        return this;
     }
 
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
+    public CompanyResource setDescription(String description) {
         this.description = description;
-    }
-
-    public String getSector() {
-        return sector;
-    }
-
-    public void setSector(String sector) {
-        this.sector = sector;
+        return this;
     }
 
     public String getLogo() {
         return logo;
     }
 
-    public void setLogo(String logo) {
+    public CompanyResource setLogo(String logo) {
         this.logo = logo;
+        return this;
     }
 
     public Long getRuc() {
         return ruc;
     }
 
-    public void setRuc(Long ruc) {
+    public CompanyResource setRuc(Long ruc) {
         this.ruc = ruc;
+        return this;
     }
 
     public String getDirección() {
         return dirección;
     }
 
-    public void setDirección(String dirección) {
+    public CompanyResource setDirección(String dirección) {
         this.dirección = dirección;
+        return this;
+    }
+
+    public Long getIdEmployeer() {
+        return idEmployeer;
+    }
+
+    public CompanyResource setIdEmployeer(Long idEmployeer) {
+        this.idEmployeer = idEmployeer;
+        return this;
+    }
+
+    public String getFirstnameEmployeer() {
+        return firstnameEmployeer;
+    }
+
+    public CompanyResource setFirstnameEmployeer(String firstnameEmployeer) {
+        this.firstnameEmployeer = firstnameEmployeer;
+        return this;
+    }
+
+    public String getLastnameEmployeer() {
+        return lastnameEmployeer;
+    }
+
+    public CompanyResource setLastnameEmployeer(String lastnameEmployeer) {
+        this.lastnameEmployeer = lastnameEmployeer;
+        return this;
+    }
+
+    public String getEmailEmployeer() {
+        return emailEmployeer;
+    }
+
+    public CompanyResource setEmailEmployeer(String emailEmployeer) {
+        this.emailEmployeer = emailEmployeer;
+        return this;
+    }
+
+    public Long getNumberEmployeer() {
+        return numberEmployeer;
+    }
+
+    public CompanyResource setNumberEmployeer(Long numberEmployeer) {
+        this.numberEmployeer = numberEmployeer;
+        return this;
+    }
+
+    public String getPasswordEmployeer() {
+        return passwordEmployeer;
+    }
+
+    public CompanyResource setPasswordEmployeer(String passwordEmployeer) {
+        this.passwordEmployeer = passwordEmployeer;
+        return this;
+    }
+
+    public String getDocumentEmployeer() {
+        return documentEmployeer;
+    }
+
+    public CompanyResource setDocumentEmployeer(String documentEmployeer) {
+        this.documentEmployeer = documentEmployeer;
+        return this;
+    }
+
+    public Long getIdSector() {
+        return idSector;
+    }
+
+    public CompanyResource setIdSector(Long idSector) {
+        this.idSector = idSector;
+        return this;
+    }
+
+    public String getNameSector() {
+        return nameSector;
+    }
+
+    public CompanyResource setNameSector(String nameSector) {
+        this.nameSector = nameSector;
+        return this;
+    }
+
+    public String getDescriptionSector() {
+        return descriptionSector;
+    }
+
+    public CompanyResource setDescriptionSector(String descriptionSector) {
+        this.descriptionSector = descriptionSector;
+        return this;
     }
 }
+
