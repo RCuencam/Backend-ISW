fcd4e3a9ed629a3b2a8fad31d4e3370132262264
Schmidt <56980361+elcrack2001@users.noreply.github.com>
2021-07-01 16:13:50 -0500

    Update PostulantJob.java

--- src/main/java/com/example/jobagapi/domain/model/PostulantJob.java
+++ src/main/java/com/example/jobagapi/domain/model/PostulantJob.java
@@ -1,45 +1,22 @@
 package com.example.jobagapi.domain.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonView;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "postulantjob")
 public class PostulantJob extends AuditModel{
-    //ID
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    public Postulant getPostulant() {
-        return postulant;
-    }
-
-    public PostulantJob setPostulant(Postulant postulant) {
-        this.postulant = postulant;
-        return this;
-    }
-
-    //Relacion con postulante
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "fk_postulant_id", nullable = false)
     @OnDelete(action = OnDeleteAction.CASCADE)
     private Postulant postulant;
-
-    public JobOffer getJobOffer() {
-        return jobOffer;
-    }
-
-    public PostulantJob setJobOffer(JobOffer jobOffer) {
-        this.jobOffer = jobOffer;
-        return this;
-    }
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "fk_joboffer_id", nullable = false)
@@ -50,11 +27,39 @@ public class PostulantJob extends AuditModel{
     @NotNull
     private boolean aceppt;
 
+    public PostulantJob() {
+    }
+
     public PostulantJob(boolean aceppt) {
         this.aceppt = aceppt;
     }
 
-    public PostulantJob() {
+
+    public Long getId() {
+        return id;
+    }
+
+    public PostulantJob setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public Postulant getPostulant() {
+        return postulant;
+    }
+
+    public PostulantJob setPostulant(Postulant postulant) {
+        this.postulant = postulant;
+        return this;
+    }
+
+    public JobOffer getJobOffer() {
+        return jobOffer;
+    }
+
+    public PostulantJob setJobOffer(JobOffer jobOffer) {
+        this.jobOffer = jobOffer;
+        return this;
     }
 
     public boolean isAceppt() {
@@ -63,15 +68,6 @@ public class PostulantJob extends AuditModel{
 
     public PostulantJob setAceppt(boolean aceppt) {
         this.aceppt = aceppt;
-        return this;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public PostulantJob setId(Long id) {
-        this.id = id;
         return this;
     }
 }
