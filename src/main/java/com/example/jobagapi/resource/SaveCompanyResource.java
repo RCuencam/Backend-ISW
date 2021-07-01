6b9197e59cee7f9e34f586cd4295c9ae2a5c990b
Schmidt <56980361+elcrack2001@users.noreply.github.com>
2021-07-01 15:17:56 -0500

    Update SaveCompanyResource.java

--- src/main/java/com/example/jobagapi/resource/SaveCompanyResource.java
+++ src/main/java/com/example/jobagapi/resource/SaveCompanyResource.java
@@ -1,62 +1,65 @@
 package com.example.jobagapi.resource;
 
-
-import com.example.jobagapi.domain.model.AuditModel;
+import javax.validation.constraints.Size;
 
 public class SaveCompanyResource {
 
-    String name;
-    String description;
-    String sector;
-    String logo;
-    Long ruc;
-    String dirección;
+    @Size(max = 100)
+    private String name;
+
+    @Size(max = 100)
+    private String description;
+
+    @Size(max = 100)
+    private String logo;
+
+    private Long ruc;
+
+    @Size(max = 100)
+    private String dirección;
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
+    public SaveCompanyResource setName(String name) {
         this.name = name;
+        return this;
     }
 
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
+    public SaveCompanyResource setDescription(String description) {
         this.description = description;
-    }
-
-    public String getSector() {
-        return sector;
-    }
-
-    public void setSector(String sector) {
-        this.sector = sector;
+        return this;
     }
 
     public String getLogo() {
         return logo;
     }
 
-    public void setLogo(String logo) {
+    public SaveCompanyResource setLogo(String logo) {
         this.logo = logo;
+        return this;
     }
 
     public Long getRuc() {
         return ruc;
     }
 
-    public void setRuc(Long ruc) {
+    public SaveCompanyResource setRuc(Long ruc) {
         this.ruc = ruc;
+        return this;
     }
 
     public String getDirección() {
         return dirección;
     }
 
-    public void setDirección(String dirección) {
+    public SaveCompanyResource setDirección(String dirección) {
         this.dirección = dirección;
+        return this;
     }
 }
