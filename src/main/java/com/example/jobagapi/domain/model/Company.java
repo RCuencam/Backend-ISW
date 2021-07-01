9452946c9bb56b845b2192fe756a8ddc412f5344
Schmidt <56980361+elcrack2001@users.noreply.github.com>
2021-07-01 15:15:12 -0500

    Update Company.java

--- src/main/java/com/example/jobagapi/domain/model/Company.java
+++ src/main/java/com/example/jobagapi/domain/model/Company.java
@@ -1,124 +1,129 @@
 package com.example.jobagapi.domain.model;
 
-
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
-import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name ="companys")
-public class Company {
+public class Company extends AuditModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotNull
-    @Length(max = 100)
+    @Size(max = 100)
     private String name;
+
     @NotNull
-    @Length(max = 100)
+    @Size(max = 100)
     private String description;
 
     @NotNull
-    @Length(max = 100)
+    @Size(max = 100)
     private String logo;
 
     private Long ruc;
 
     @NotNull
-    @Length(max = 100)
+    @Size(max = 100)
     private String dirección;
 
     @OneToOne( fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "employeer_id", nullable = false)
+    @JoinColumn(name = "fk_employeer_id", nullable = false)
     @OnDelete(action = OnDeleteAction.CASCADE)
     private Employeer employeer;
 
     @ManyToOne( fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "sector_id", nullable = false)
+    @JoinColumn(name = "fk_sector_id", nullable = false)
     @OnDelete(action = OnDeleteAction.CASCADE)
     private Sector sector;
 
+    public Company() {
+    }
 
-    public Company(Long id, String name, String description,  String logo, Long ruc, String dirección, Employeer employeer,Sector sector) {
+    public Company(Long id, String name, String description, String logo, Long ruc, String dirección) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.logo = logo;
         this.ruc = ruc;
         this.dirección = dirección;
-        this.employeer = employeer;
-        this.sector =sector;
     }
 
-    public Company(){}
 
     public Long getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public Company setId(Long id) {
         this.id = id;
+        return this;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
+    public Company setName(String name) {
         this.name = name;
+        return this;
     }
 
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
+    public Company setDescription(String description) {
         this.description = description;
+        return this;
     }
-
-
 
     public String getLogo() {
         return logo;
     }
 
-    public void setLogo(String logo) {
+    public Company setLogo(String logo) {
         this.logo = logo;
+        return this;
     }
 
     public Long getRuc() {
         return ruc;
     }
 
-    public void setRuc(Long ruc) {
+    public Company setRuc(Long ruc) {
         this.ruc = ruc;
+        return this;
     }
 
     public String getDirección() {
         return dirección;
     }
 
-    public void setDirección(String dirección) {
+    public Company setDirección(String dirección) {
         this.dirección = dirección;
+        return this;
     }
 
     public Employeer getEmployeer() {
         return employeer;
     }
 
-    public void setEmployeer(Employeer employeer) {
+    public Company setEmployeer(Employeer employeer) {
         this.employeer = employeer;
+        return this;
     }
 
     public Sector getSector() {
         return sector;
     }
 
-    public void setSector(Sector sector) {
+    public Company setSector(Sector sector) {
         this.sector = sector;
+        return this;
     }
 }
