c767e6b8d4839c59a80ae634c5be661d1e0fdc49
Schmidt <56980361+elcrack2001@users.noreply.github.com>
2021-07-01 15:16:47 -0500

    Update CompanyService.java

--- src/main/java/com/example/jobagapi/domain/service/CompanyService.java
+++ src/main/java/com/example/jobagapi/domain/service/CompanyService.java
@@ -4,16 +4,13 @@ import com.example.jobagapi.domain.model.Company;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Repository;
 
 
 public interface CompanyService {
-    Page<Company> getAllCompanysByEmployeerId(Long employeerId, Pageable pageable);
-    Company getCompanyByIdAndEmployeerId(Long employeerId, Long companysId);
-    Page<Company> getAllCompanysBySectorId(Long sectorId, Pageable pageable);
-    Company getCompanyByIdAndSectorId(Long sectorId, Long companysId);
 
-    Company createCompany(Long employeerId, Company company);
-    Company updateCompany(Long employeerId, Long companyId, Company companyDetails);
-    ResponseEntity<?> deleteCompany(Long employeerId,  Long companyId);
+    Company createCompany(Long employeerId, Long sectorId, Company companyRequest);
+    Company updateCompany(Long employeerId, Long sectorId, Company companyRequest);
+    ResponseEntity<?> deleteCompany(Long employeerId, Long sectorId);
+    Company getCompanyById(Long companyId);
+    Page<Company> getAllCompany(Pageable pageable);
 }
