f46a814179810a7f678b57fba8941c1b0477d952
Caro2870 <55043767+Caro2870@users.noreply.github.com>
2021-07-02 19:02:11 -0500

    Update PostulantJobController.java

--- src/main/java/com/example/jobagapi/controller/PostulantJobController.java
+++ src/main/java/com/example/jobagapi/controller/PostulantJobController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 import java.util.stream.Collectors;
-
+/**/
 @RestController
 @CrossOrigin
 @RequestMapping("/api")
